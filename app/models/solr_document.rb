2003f7ef7f4e2329662e80264959144c25305c15
Adam McKenzie <adam.mckenzie@usask.ca>
2022-01-14 13:55:17 -0600

    RDM-5321 Add helper functions for getting geo values from document

--- app/models/solr_document.rb
+++ app/models/solr_document.rb
@@ -1,7 +1,15 @@
 # frozen_string_literal: true
 class SolrDocument
   include Blacklight::Solr::Document
- include Geoblacklight::SolrDocument
+  include Geoblacklight::SolrDocument
+
+  def initialize(source_doc = {}, response = nil)
+    super(source_doc, response)
+    @bboxes = ['Stanley Park; Vancouver; British Columbia; Canada', '123.72, 49.195, -123.020, 49.315']
+    @lines = ['123,45, 49.195, -123.45, 49.195', '123.72, 49.195, -123.020, 49.315']
+    @points = ['123.72,49.50']
+    @polygons = ['123,45, 49.195, 92.321, 35.323, 87.232, 23.231, -123.45, 49.195', '123.72, 49.195, -123.020, 49.315, 122.12, 87.321']
+  end
 
   # self.unique_key = 'id'
   self.unique_key = 'layer_slug_s'
@@ -18,4 +26,69 @@ class SolrDocument
   # and Blacklight::Document::SemanticFields#to_semantic_values
   # Recommendation: Use field names from Dublin Core
   use_extension(Blacklight::Document::DublinCore)
+
+  def has_bbox?
+    @bboxes.length() > 0
+  end
+
+  def bbox_all_id
+    'bbox-all'
+  end
+
+  def bbox_prefix
+    'bbox-'
+  end
+
+  def bboxes
+    @bboxes
+  end
+
+  def has_line?
+    @lines.length() > 0
+  end
+
+  def line_all_id
+    'line-all'
+  end
+
+  def line_prefix
+    'line-'
+  end
+
+  def lines
+    @lines
+  end
+
+  def has_point?
+    @points.length() > 0
+  end
+
+  def point_all_id
+    'point-all'
+  end
+
+  def point_prefix
+    'point-'
+  end
+
+  def points
+    @points
+  end
+
+  def has_polygon?
+    @polygons.length() > 0
+  end
+
+  def polygon_all_id
+    'polygon-all'
+  end
+
+  def polygon_prefix
+    'polygon-'
+  end
+
+  def polygons
+    @polygons
+  end
+
 end
