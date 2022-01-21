a9727ec0f483bee9d20307d3b0bc7f032f810f53
Adam McKenzie <adam.mckenzie@usask.ca>
2022-01-20 20:11:44 -0600

    RDM-5327 Add citations for landing page

--- app/helpers/geodisy_helper.rb
+++ app/helpers/geodisy_helper.rb
@@ -15,4 +15,25 @@ module GeodisyHelper
       link_to (fa_icon "external-link", text: args[:document].references.url.endpoint, right: true), args[:document].references.url.endpoint
     end
   end
+
+  def get_document_bibtex(document)
+    currentDoc = BibTeX::Entry.new({
+      :bibtex_type => :misc,
+      :bibtex_key => :currentDoc})
+    currentDoc.author = document.fetch('dc_creator_sm', nil).join(' and ') if document.has?('dc_creator_sm')
+    currentDoc.title = document.fetch('dc_title_s') if document.has?('dc_title_s')
+    currentDoc.issued = document.fetch('dct_issued_s') if document.has?('dc_issued_s')
+    currentDoc.howpublished ='{\\url{' + document.fetch('dc_identifier_s') + '}}' if document.has?('dc_identifier_s')
+
+    bib = BibTeX::Bibliography.new
+    bib << currentDoc
+    return bib
+  end
+
+  def get_apa_citation(document)
+    bib = get_document_bibtex(document)
+    cp = CiteProc::Processor.new style: 'apa', format: 'html'
+    cp.register bib[:currentDoc].to_citeproc
+    return cp.render :bibliography, id: 'currentDoc'
+  end
 end
