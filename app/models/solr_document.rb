cc0a6184c16b7400da009de3d87d0eff6f476177
Paul Dante <paul.dante@ubc.ca>
2022-01-30 23:27:11 -0800

    Ok, moving it seemed to help, moved past sanity check

--- app/models/solr_document.rb
+++ app/models/solr_document.rb
@@ -60,14 +60,13 @@ class SolrDocument
   end
 
   def get_bboxes
-       #bs = fetch(Settings.FIELDS.BBOXES)
-       #array = []
-       #for b in bs do
-       #    if b.bbox_type = "bounding box"
-       #        array.push(b.north + ", " + b.west + ", " + b.south + ', ' + b.east)
-       #    end
-       #end
-       #return array
-       "this worked"
+       bs = fetch(Settings.FIELDS.BBOXES)
+       array = []
+       for b in bs do
+           if b.bbox_type = "bounding box"
+               array.push(b.north + ", " + b.west + ", " + b.south + ', ' + b.east)
+           end
+       end
+       return array
   end
 end
