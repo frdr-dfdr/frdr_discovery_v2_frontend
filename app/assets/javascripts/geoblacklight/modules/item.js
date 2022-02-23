28596a4e3921dd4532d3115289b832b4bceec2db
Paul Dante <paul.dante@ubc.ca>
2022-02-23 11:55:30 -0800

    Fixing element call

--- app/assets/javascripts/geoblacklight/modules/item.js
+++ app/assets/javascripts/geoblacklight/modules/item.js
@@ -2,7 +2,7 @@ Blacklight.onLoad(function() {
   $('[data-map="item"]').each(function(i, element) {
 
     // get viewer module from protocol value and capitalize to match class name
-    var viewerName = $element.data().protocol,
+    var viewerName = $(element).data().protocol,
       viewer;
 
     // get new viewer instance and pass in element
