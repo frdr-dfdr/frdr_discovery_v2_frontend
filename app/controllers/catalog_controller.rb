459719ec53e957da5c6382db030fa44d3f77c35c
Adam McKenzie <adam.mckenzie@usask.ca>
2022-03-18 09:47:49 -0600

    RDM-5667 Change default number of rows to 20 and remove per page control

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -23,7 +23,7 @@ class CatalogController < ApplicationController
 
     ## Default rows returned from Solr
     ## @see https://lucene.apache.org/solr/guide/6_6/common-query-parameters.html
-    config.default_per_page = 10
+    config.default_per_page = 20
 
     ## Default parameters to send on single-document requests to Solr. These settings are the Blackligt defaults (see SolrHelper#solr_doc_params) or
     ## parameters included in the Blacklight-jetty document requestHandler.
@@ -254,7 +254,6 @@ class CatalogController < ApplicationController
 
     # Tools from Blacklight
     config.add_results_collection_tool(:sort_widget)
-    config.add_results_collection_tool(:per_page_widget)
 
     # Custom tools for GeoBlacklight
     config.add_show_tools_partial :metadata, if: proc { |_context, _config, options| options[:document] && (Settings.METADATA_SHOWN & options[:document].references.refs.map(&:type).map(&:to_s)).any? }
