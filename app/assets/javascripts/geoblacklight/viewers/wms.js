52f26cc3a91f2fd5070f70048cd89623c1c98b91
Paul Dante <paul.dante@ubc.ca>
2022-04-04 00:38:46 -0700

    added missing commas in list

--- app/assets/javascripts/geoblacklight/viewers/wms.js
+++ app/assets/javascripts/geoblacklight/viewers/wms.js
@@ -26,7 +26,7 @@ GeoBlacklight.Viewer.Wms = GeoBlacklight.Viewer.Map.extend({
       tiled: true,
       CRS: 'EPSG:900913',
       opacity: this.options.opacity,
-      detectRetina: _this.detectRetina()
+      detectRetina: _this.detectRetina(),
       id: "_preview_"
     });
     removeSingleBoundsOverlay("_preview_");
@@ -44,7 +44,7 @@ GeoBlacklight.Viewer.Wms = GeoBlacklight.Viewer.Map.extend({
         tiled: true,
         CRS: 'EPSG:900913',
         opacity: this.options.opacity,
-        detectRetina: _this.detectRetina()
+        detectRetina: _this.detectRetina(),
         id: "_preview_"
       });
       removeSingleBoundsOverlay("_preview_");
