7f6d65d3115622679f577467fadd53aa4fd808fb
Paul Dante <paul.dante@ubc.ca>
2022-04-04 01:23:47 -0700

    try this with require above

--- app/assets/javascripts/geoblacklight/modules/item.js
+++ app/assets/javascripts/geoblacklight/modules/item.js
@@ -180,7 +180,7 @@ Blacklight.onLoad(function() {
             var file = $(".download-select option:selected");
             var text = $(".download-select option:selected").text();
             var val = $(".download-select option:selected").val();
-            addPreviewLayer(val);
+            this.addPreviewLayer(val);
           });
 
   });
