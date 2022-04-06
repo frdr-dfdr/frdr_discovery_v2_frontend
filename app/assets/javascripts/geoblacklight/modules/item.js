268d76c0f64e49ec010f9b499bf179dfda416dde
Paul Dante <paul.dante@ubc.ca>
2022-04-06 13:13:10 -0700

    viewer.addpreview

--- app/assets/javascripts/geoblacklight/modules/item.js
+++ app/assets/javascripts/geoblacklight/modules/item.js
@@ -181,7 +181,7 @@ Blacklight.onLoad(function() {
             var file = $(".download-select option:selected");
             var text = $(".download-select option:selected").text();
             var val = $(".download-select option:selected").val();
-            addPreviewLayer(val);
+            viewer.addPreviewLayer(val);
           });
 
   });
