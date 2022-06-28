319e2e1953768537fb86c84d58c7d134340eafa9
Paul Dante <paul.dante.ubc@gmail.com>
2022-06-28 14:52:35 -0700

    Gathering results search info and posting to globus to get records

--- app/assets/javascripts/geoblacklight/modules/results.js
+++ app/assets/javascripts/geoblacklight/modules/results.js
@@ -61,11 +61,25 @@ Blacklight.onLoad(function() {
        // add geosearch control to map
        geoblacklight.map.addControl(L.control.geosearch(opts));
        var pruneCluster = new PruneClusterForLeaflet();
-       repo = []
-       $("input:hidden[name='f[dct_provenance_s][]']").map(function(x, elm) { return repo.add(elm.value); });
-       $("input:hidden[name='f[dct_provenance_s][]']").map(function(x, elm) { return repo.add(elm.value); });
+       repos = []
+       perms = []
+       authors = []
+       $("input:hidden[name='f[dct_provenance_s][]']").map(function(x, elm) { return repos.add(elm.value); });
+       $("input:hidden[name='f[dc_rights_s][]']").map(function(x, elm) { return perms.add(elm.value); });
+       $("input:hidden[name='f[dc_creator_sm][]']").map(function(x, elm) { return authors.add(elm.value); });
        year_begin = $("input:hidden[name='range[gbl_indexYear_im][begin]'").value;
+       if(year_begin==null){
+            year_begin = "*";
+       }
        year_end = $("input:hidden[name='range[gbl_indexYear_im][end]'").value;
+       if(year_end == null){
+            year_end == "*";
+       }
+       bbox = $("input:hidden[name='bbox']")[0].value.split(' ');
+       if(bbox == null)
+            bbox == [];
+       var q = $("#q[name='q']")[0].value
+       results = getGlobusRecords(q,repos,perms,authors,year_begin,year_end, bbox);
        
 
 
@@ -73,11 +87,11 @@ Blacklight.onLoad(function() {
             // Not usingURL() to maintain legacy IE support
             url = document.createElement('a');
             loc = window.location.href;
-            loc.replace('https','http');
-            loc.replace('/en',':8983/solr/geoblacklight/select')
-            loc.replace('/fr',':8983/solr/geoblacklight/select')
+            loc = loc.replace('https','http');
+            loc = loc.replace('/en',':8983/solr/geoblacklight/select')
+            loc = loc.replace('/fr',':8983/solr/geoblacklight/select')
             // Oboe - Re-query Solr for JSON results
-            oboe(url.toString() + '&format=json&per_page=1000&rows=1000')
+            oboe(loc + '&format=json&per_page=1000&rows=1000')
               .node('response.*', function( doc ){
                   if(typeof doc['solr_geom'] != 'undefined'){
                       geom = doc['solr_geom']
@@ -122,6 +136,87 @@ Blacklight.onLoad(function() {
         $('#map').after($doc.find('#map').next());
       }
     });
+  }
+
+  function getGlobusRecords(q,repos,perms,authors,year_begin,year_end, bbox){
+    var base = {};
+    base["@datatpe"] = "GSearchRequest";
+    base["@version"] = "2017-09-01";
+    base["advanced"] = true;
+    base["limit"] = 20;
+    base["offset"] = 0;
+    if(q != null)
+        base["q"] = q;
+    base["result_format_version"] = "2017-09-01";
+    var filters = [];
+    var filter = {}
+    filter["@datatype"] = "GFilter";
+    filter["@version"] = "2017-09-01";
+
+    //repos facet
+    if(repos != []){
+        filter["field_name"] = "dct_provenance_s";
+        filter["type"] = "match_any";
+        filter["values"] = repos;
+        var repoFilter = filter;
+        filters.add(repoFilter);
+    }
+    //author facet
+    if(authors != []){
+        filter["field_name"] = "dc_creator_sm";
+        filter["values"] = authors;
+        filter["type"] = "match_any";
+        var authorFilter = filter;
+        filters.add(authorFilter);
+    }
+    //permissions facet
+    if(perms != []){
+        filter["field_name"] = "dc_rights_s";
+            filter["type"] = "match_any";
+            filter["values"] = perms;
+            var permsFilter = filter;
+            filters.add(premsFilter);
+    }
+
+    //date range facet
+    filter["type"] = "range";
+    filter["field_name"] = "dct_issued_s";
+    var vals = {}
+    vals["from"] = year_begin;
+    vals["to"] = year_end;
+    filter["values"] = vals;
+    var dates = filter;
+    filters.add(dates);
+
+    //bounding box facet
+    filter["type"] = "geo_bounding_box";
+    if(bbox != []){
+        filter["field_name"] = "geoLocationPolygons";
+        filter["type"] = "geo_bounding_box";
+        var bottomRight = {};
+        bottomRight["lat"] = bbox[3];
+        bottomRight["lon"] = bbox[1];
+        filter["bottom_right"] = bottomRight;
+        var topLeft = {};
+        topLeft["lat"] = bbox[2];
+        topLeft["lon"] = bbox[0];
+        filter["top_left"] = topLeft;
+        var geoFacet = filter;
+        filters.add(geoFacet);
+    }
+    base["filters"] = filters;
+    var xhr = new XMLHttpRequest();
+    var url = "https://search.api.globus.org/v1/index/29abfeb0-bd17-4e6b-b058-85ea7a975e0f/search";
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            var json = JSON.parse(xhr.responseText);
+            console.log(json.email + ", " + json.password);
+        }
+    };
+    var response = xhr.send(base);
+    var text =  response.string();
   }
 });
 
