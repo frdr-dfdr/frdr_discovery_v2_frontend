e5f50fb0dd8462fc2305dace60da0affbb08206d
Paul Dante <paul.dante.ubc@gmail.com>
2022-06-30 12:49:40 -0700

    try forcing map to move

--- app/assets/javascripts/geoblacklight/modules/results.js
+++ app/assets/javascripts/geoblacklight/modules/results.js
@@ -184,8 +184,11 @@ Blacklight.onLoad(function() {
     const url = "https://search.api.globus.org/v1/index/29abfeb0-bd17-4e6b-b058-85ea7a975e0f/search";
     pruneCluster = await updatePrune(url,base,pruneCluster);
     geoblacklight.map.addLayer(pruneCluster);
-    geoblacklight.map.getBounds();
-    
+    let orig = geoblacklight.map.getBounds();
+    let nL = orig['_northEast']['lat'];
+    let eLg = orig['_northEast']['lng'];
+    geoblacklight.map.bounds(orig);
+
   }
 
   function addRecordsToClusters(json, pruneCluster){
