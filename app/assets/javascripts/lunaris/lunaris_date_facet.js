3aeb3ab9a7e74508bf90c99724ebe156912b1a91
Adam McKenzie <adam.mckenzie@usask.ca>
2022-07-05 17:34:07 -0600

    RDM-5762 Add bootstrap datepicker to pick to and from dates

--- app/assets/javascripts/lunaris/lunaris_date_facet.js
+++ app/assets/javascripts/lunaris/lunaris_date_facet.js
@@ -1,4 +1,25 @@
+//= require bootstrap-datepicker.min.js
+//= require bootstrap-datepicker-en-CA.min.js
+//= require bootstrap-datepicker.fr.min.js
+
 Blacklight.onLoad(function() {
+    var lang = "en_CA";
+    if (window.location.pathname.endsWith('/fr'))
+    {
+        lang = "fr";
+    }
+    var datePickerOpts = {
+        autoclose: true,
+        clearBtn: true,
+        format: 'yyyy-mm-dd',
+        immediateUpdates: true,
+        language: lang,
+        todayBtn: true
+    };
+
+    $('#date_from').datepicker(datePickerOpts);
+
+    $('#date_to').datepicker(datePickerOpts);
 
     var form = $('#filter-date');
     form.submit(function(event) {
